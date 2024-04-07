104904dd7a9f1c0f243e091fa7d66f28e61bab57
jazbang <jbang@udesa.edu.ar>
2024-04-07 17:26:29 -0300

    Del punto 1, archivos html cambiados a ejs, rutas creadas (por ahora en comentarios)

--- proyectoIntegrador/routes/index.js
+++ proyectoIntegrador/routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//router.get('/', productController.index);
+//router.get('/login', productController.login);
+//router.get('/product', productController.productos);
+//router.get('/resultadosDeBusqueda', productController.resultadosDeBusqueda);
+//router.get('/register', productController.register);
+//router.get('/profile', productController.profile);
+//router.get('/profileEdit', productController.editar);
+//router.get('/addProduct', productController.producto);
+
+
 module.exports = router;

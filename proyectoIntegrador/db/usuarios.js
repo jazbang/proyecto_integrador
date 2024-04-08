b8d1fd4fae59ab9096be62570e3a687e2f00598a
mbeorlegui@udesa.edu.ar <mbeorlegui@udesa.edu.ar>
2024-04-08 10:41:32 -0300

    hice estructura del modulo creado por nosotros en el archivo usuarios.js

---
+++ proyectoIntegrador/db/usuarios.js
@@ -0,0 +1,19 @@
+const usuarios={
+    usuario:{
+        email:'martinaperez@gmail.com',
+        usuario: 'martinaperez',
+        contraseña: 'martina2004',
+        fechaNacimiento: '26/02/2004',
+        documento: 46442731,
+    },
+    productos:[
+        {
+        comentarios:[{
+            nombreDeUsuario: '',
+            textoDelComentario:'',
+            imagenDePerfil:'agregar link de la carpeta del protecto'
+        }]
+    } ],
+}
+
+module.exports=usuarios;

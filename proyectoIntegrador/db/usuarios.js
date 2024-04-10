2328b0598742f415ae7fe5410f2f0efe4c8b62b7
mbeorlegui@udesa.edu.ar <mbeorlegui@udesa.edu.ar>
2024-04-10 11:40:00 -0300

    termine de completar el archivo usuarios.js

--- proyectoIntegrador/db/usuarios.js
+++ proyectoIntegrador/db/usuarios.js
@@ -12,9 +12,9 @@ const usuarios={
         nombreDelProducto:'Remera Nike',
         descripcion: 'Remera Nike sportswear unisex algodón 100%.',
         comentarios:[{
-            nombreDeUsuario: '',
-            textoDelComentario:'',
-            imagenDePerfil:'agregar link de la carpeta del protecto'
+            nombreDeUsuario: 'matiasfernandez',
+            textoDelComentario:'Muy buena calidad.',
+            imagenDePerfil:'file:///var/folders/9l/ngx1v7v17x76cy3h1yk5bw1r0000gn/T/TemporaryItems/NSIRD_screencaptureui_RSnJjt/Captura%20de%20pantalla%202024-04-08%20a%20la(s)%2017.37.52.png'
         }]
         },
         {
@@ -22,18 +22,18 @@ const usuarios={
             nombreDelProducto:'Mom Jeans',
             descripcion: 'Mom jeans denim azul oscuro',
             comentarios:[{
-                nombreDeUsuario: '',
-                textoDelComentario:'',
-                imagenDePerfil:'agregar link de la carpeta del protecto'
+                nombreDeUsuario: 'karinarodriguez',
+                textoDelComentario:'Volvería a comprar definitivamente.',
+                imagenDePerfil:'file:///var/folders/9l/ngx1v7v17x76cy3h1yk5bw1r0000gn/T/TemporaryItems/NSIRD_screencaptureui_SeTzjm/Captura%20de%20pantalla%202024-04-08%20a%20la(s)%2017.38.28.png'
             }],
         },{
             imagen:'/imagenes/products/babytee.png',
             nombreDelProducto:'Top',
             descripcion: 'Top blanco y rosa con estampa 100% algodón.',
             comentarios:[{
-                nombreDeUsuario: '',
-                textoDelComentario:'',
-                imagenDePerfil:'agregar link de la carpeta del protecto'
+                nombreDeUsuario: 'omardiaz',
+                textoDelComentario:'Recomiendo totalmente.',
+                imagenDePerfil:'file:///var/folders/9l/ngx1v7v17x76cy3h1yk5bw1r0000gn/T/TemporaryItems/NSIRD_screencaptureui_aZv94S/Captura%20de%20pantalla%202024-04-08%20a%20la(s)%2017.42.14.png'
             }],
         },{
             imagen:'/imagenes/products/buzo.png',
@@ -49,54 +49,54 @@ const usuarios={
             nombreDelProducto:' Pollera',
             descripcion: 'Pollera denim upcycling reversionado.',
             comentarios:[{
-                nombreDeUsuario: '',
-                textoDelComentario:'',
-                imagenDePerfil:'agregar link de la carpeta del protecto'
+                nombreDeUsuario: 'micaelabili',
+                textoDelComentario:'Muy comoda.',
+                imagenDePerfil:'file:///var/folders/9l/ngx1v7v17x76cy3h1yk5bw1r0000gn/T/TemporaryItems/NSIRD_screencaptureui_0d8TdY/Captura%20de%20pantalla%202024-04-10%20a%20la(s)%2011.34.04.png'
             }],
         },{
             imagen:'/imagenes/products/campera.png',
             nombreDelProducto:'Campera',
             descripcion: 'Campera negra puffer de The North Face.',
             comentarios:[{
-                nombreDeUsuario: '',
-                textoDelComentario:'',
-                imagenDePerfil:'agregar link de la carpeta del protecto'
+                nombreDeUsuario: 'estimiedziak',
+                textoDelComentario:'La mejor calidad.',
+                imagenDePerfil:'file:///var/folders/9l/ngx1v7v17x76cy3h1yk5bw1r0000gn/T/TemporaryItems/NSIRD_screencaptureui_tGl6lp/Captura%20de%20pantalla%202024-04-08%20a%20la(s)%2017.44.12.png'
             }],
         },{
             imagen:'/imagenes/products/musculosa.png',
             nombreDelProducto:'Musculosa',
             descripcion: 'Musculosa básica negra morely.',
             comentarios:[{
-                nombreDeUsuario: '',
-                textoDelComentario:'',
-                imagenDePerfil:'agregar link de la carpeta del protecto'
+                nombreDeUsuario: 'andrearasligui',
+                textoDelComentario:'Me encantó el material.',
+                imagenDePerfil:'file:///var/folders/9l/ngx1v7v17x76cy3h1yk5bw1r0000gn/T/TemporaryItems/NSIRD_screencaptureui_QGvZCz/Captura%20de%20pantalla%202024-04-10%20a%20la(s)%2011.35.08.png'
             }],
         },{
             imagen:'/imagenes/products/parachutte.png',
             nombreDelProducto:'Parachuttes',
             descripcion: 'Pantalón parachutte verde militar.',
             comentarios:[{
-                nombreDeUsuario: '',
-                textoDelComentario:'',
-                imagenDePerfil:'agregar link de la carpeta del protecto'
+                nombreDeUsuario: 'biancadelre',
+                textoDelComentario:'Divino el color.',
+                imagenDePerfil:'file:///var/folders/9l/ngx1v7v17x76cy3h1yk5bw1r0000gn/T/TemporaryItems/NSIRD_screencaptureui_Oh85Da/Captura%20de%20pantalla%202024-04-10%20a%20la(s)%2011.36.16.png'
             }],
         },{
             imagen:'/imagenes/products/saco.png',
             nombreDelProducto:'Saco',
             descripcion: 'Saco negro de vestir.',
             comentarios:[{
-                nombreDeUsuario: '',
-                textoDelComentario:'',
-                imagenDePerfil:'agregar link de la carpeta del protecto'
+                nombreDeUsuario: 'valeriagonzales',
+                textoDelComentario:'Me encanta el material, muy comodo.',
+                imagenDePerfil:'file:///var/folders/9l/ngx1v7v17x76cy3h1yk5bw1r0000gn/T/TemporaryItems/NSIRD_screencaptureui_Z8fnHe/Captura%20de%20pantalla%202024-04-10%20a%20la(s)%2011.37.20.png'
             }],
         },{
             imagen:'/imagenes/products/bermuda.png',
             nombreDelProducto:'Bermudas de Jean',
             descripcion: 'Bermuda de jean azul.',
             comentarios:[{
-                nombreDeUsuario: '',
-                textoDelComentario:'',
-                imagenDePerfil:'agregar link de la carpeta del protecto'
+                nombreDeUsuario: 'juanlopez',
+                textoDelComentario:'Recomiendo.',
+                imagenDePerfil:'file:///var/folders/9l/ngx1v7v17x76cy3h1yk5bw1r0000gn/T/TemporaryItems/NSIRD_screencaptureui_DnE8vH/Captura%20de%20pantalla%202024-04-10%20a%20la(s)%2011.38.39.png'
             }],
         }, ]
         

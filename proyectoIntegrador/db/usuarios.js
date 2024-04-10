2a4cb9d405bcac05e6cc50e35acbbc71abc405b6
mbeorlegui@udesa.edu.ar <mbeorlegui@udesa.edu.ar>
2024-04-09 21:13:02 -0300

    sigo completando el archivo usuarios.js

--- proyectoIntegrador/db/usuarios.js
+++ proyectoIntegrador/db/usuarios.js
@@ -27,44 +27,44 @@ const usuarios={
                 imagenDePerfil:'agregar link de la carpeta del protecto'
             }],
         },{
-            imagen:'',
-            nombreDelProducto:'',
-            descripcion: '',
+            imagen:'/imagenes/products/babytee.png',
+            nombreDelProducto:'Baby Tee',
+            descripcion: 'Hawaii Ringer Baby Tee Pink',
             comentarios:[{
                 nombreDeUsuario: '',
                 textoDelComentario:'',
                 imagenDePerfil:'agregar link de la carpeta del protecto'
             }],
         },{
-            imagen:'',
-            nombreDelProducto:'',
-            descripcion: '',
+            imagen:'/imagenes/products/buzo.png',
+            nombreDelProducto:' Buzo Essentials',
+            descripcion: 'Buzo Essentials blanco algodón 100%',
             comentarios:[{
                 nombreDeUsuario: '',
                 textoDelComentario:'',
                 imagenDePerfil:'agregar link de la carpeta del protecto'
             }],
         },{
-            imagen:'',
-            nombreDelProducto:'',
-            descripcion: '',
+            imagen:'/imagenes/products/polleraJean.png',
+            nombreDelProducto:' Pollera de Jean',
+            descripcion: 'Pollera de Jean personalizada negra con tachas',
             comentarios:[{
                 nombreDeUsuario: '',
                 textoDelComentario:'',
                 imagenDePerfil:'agregar link de la carpeta del protecto'
             }],
         },{
-            imagen:'',
-            nombreDelProducto:'',
-            descripcion: '',
+            imagen:'/imagenes/products/campera.png',
+            nombreDelProducto:'Campera The North Face',
+            descripcion: 'Campera nefra puffer de The North Face unisex',
             comentarios:[{
                 nombreDeUsuario: '',
                 textoDelComentario:'',
                 imagenDePerfil:'agregar link de la carpeta del protecto'
             }],
         },{
-            imagen:'',
-            nombreDelProducto:'',
+            imagen:'/imagenes/products/bermuda.png',
+            nombreDelProducto:'Bermudas de Jean',
             descripcion: '',
             comentarios:[{
                 nombreDeUsuario: '',

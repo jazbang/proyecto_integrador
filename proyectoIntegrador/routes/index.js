9825d06c8c31b9c1019f13e78eed6c6c819289c8
jazbang <jbang@udesa.edu.ar>
2024-04-15 15:12:15 -0300

    no se rompe formulario de agregar comentarios

--- proyectoIntegrador/routes/index.js
+++ proyectoIntegrador/routes/index.js
@@ -2,13 +2,9 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController')
 
-/* GET home page. */
-router.get('/',productController.index);
-
-//router.get('/', productController.index);
+router.get('/', productController.index);
 router.get('/product', productController.products);
-//router.get('/resultadosDeBusqueda', productController.resultadosDeBusqueda);
 router.get('/add', productController.agregarProducto);
+//router.get('/resultadosDeBusqueda', productController.resultadosDeBusqueda);
 
-
-module.exports = router;
+module.exports=router;

65f5fa6e66f76aa9dab12bbd1339f4e8585e1f6c
mbeorlegui@udesa.edu.ar <mbeorlegui@udesa.edu.ar>
2024-06-09 21:04:43 -0300

    trate de solucionar errores

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const productController = require('../controllers/productController')
 
 router.get('/', productController.index);
+
 router.get('/product', productController.products);
 router.get('/add', productController.agregarProducto);
 router.get('/search-results', productController.searchResults);

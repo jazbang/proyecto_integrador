a6cd147fc657297a594cb1d2d0f363772b64a38c
jazbang <jbang@udesa.edu.ar>
2024-06-14 20:49:41 -0300

    correcciones y modificaciones de la base de datos

---
+++ db/modificaciones1.sql
@@ -0,0 +1,5 @@
+USE proyectoIntegrador;
+ALTER TABLE usuarios CHANGE contraseña contrasenia VARCHAR(100);
+ALTER TABLE productos CHANGE descripción descripcion VARCHAR(100);
+USE proyectoIntegrador;
+ALTER TABLE productos CHANGE descripción descripcion VARCHAR(100);

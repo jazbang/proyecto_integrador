6f16d68a028c7d4389cb8a405155f05115d26d6b
jazbang <jbang@udesa.edu.ar>
2024-06-21 18:01:47 -0300

    volví a poner el controlador de comentarios como lo había hecho, alguien sin querer me cambió el código

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -4,41 +4,25 @@ let {validationResult} = require('express-validator');
 
 const commentController = {
     comments: function(req, res) {
-        let errors = validationResult(req);
-        if (errors.isEmpty()) {
-            let comentario = {
-                id_usuarios: req.session.user.id,
-                id_productos: req.params.id,
-                comentario: req.body.comentario
-            }
+        let productId = req.params.id;
 
-            comment.create(comentario)
-                .then((result) => {
-                    return res.redirect("/product/" + req.params.id);
-                }).catch((error) => {
-                    console.log(error);
-                });
-        } else {
-            let productId = req.params.id;
-
-            let filtrado = {
-                where: {
-                    id_productos: productId
-                },
-                include: [
-                    {association: 'producto'},
-                    {association:'usuario'}
-                ]
-            }
-    
-            comment.findAll(filtrado)
-                .then(function(data){
-                    return res.render('product', {comments: data})
-                })
-                .catch(function(error){
-                    console.log(error);
-                });   
+        let filtrado = {
+            where: {
+                id_productos: productId
+            },
+            include: [
+                {association:'usuario'}
+            ],
+            order:[['created_at', 'DESC']]
         }
+    
+        comment.findAll(filtrado)
+            .then(function(data){
+                return res.render('product', {comments: data})
+            })
+            .catch(function(error){
+                console.log(error);
+            });   
     },
     addComment:function(req,res){
         let errors= validationResult(req);

f6a51783606ddcebc0154cd7021ad703f5d60b27
jazbang <jbang@udesa.edu.ar>
2024-06-24 10:45:22 -0300

    me olvide de borrar algo de profile que deje en comentario

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -24,22 +24,6 @@ const usersController = {
             .catch(function(error){
                 return console.log(error)
             })
-        //let email= req.session.email
-        //let filtrado = {
-            //where: {email: email},
-            //include:[
-                //{association:'productos',
-                //include:{association:'comentarios'}}
-            //]
-        //}
-        //user.findOne(filtrado)
-        //.then(function(results){
-            //let productos= results.productos;
-            //return res.render('profile', {productos: productos.reverse()})
-        //})
-        //.catch(function(error){
-            //return console.log(error)
-        //})
     },
     register: function(req,res){
         return res.render('register');
